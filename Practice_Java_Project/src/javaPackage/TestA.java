9d4798b77788fb9d2e4025eb33d00c7a006708b1
Sonam1530 <Sonamsawarkar382@gmail.com>
2024-12-30 23:55:11 +0530

    After checking commit

--- Practice_Java_Project/src/javaPackage/TestA.java
+++ Practice_Java_Project/src/javaPackage/TestA.java
@@ -29,7 +29,7 @@ public class TestA {
 			System.out.println(ele.getText());
 		}
 
-	//	System.out.println("My Name is Sachin");
+		System.out.println("My Name is Sachin");
 		
 	//	System.out.println("My New Changes on this code");
 		
