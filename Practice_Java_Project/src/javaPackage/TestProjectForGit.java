b03ff8aede55563c2a87a78835a46c6991b5e815
Sonam1530 <Sonamsawarkar382@gmail.com>
2024-12-31 13:02:09 +0530

    This changes for sonam

---
+++ Practice_Java_Project/src/javaPackage/TestProjectForGit.java
@@ -0,0 +1,12 @@
+package javaPackage;
+
+public class TestProjectForGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My Name is Sachin");
+
+	}
+
+}
